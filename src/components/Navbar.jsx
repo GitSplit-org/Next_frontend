ae2a6a32f42c9dda54992d0dd7eadffdac668c40
Punarv Pawade <punarvpawade17@gmail.com>
2024-01-19 02:09:50 +0530

    github auth tidy

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
-
-const Navbar = () => {
+import Link from "next/link";
+import { getServerSession } from "next-auth/next";
+import Image from "next/image";
+import { authOptions } from "./auth";
+authOptions;
+const Navbar = async () => {
+  const session = await getServerSession(authOptions);
   return (
     <>
       <nav class="bg-white border-gray-200 dark:bg-gray-900">
@@ -10,53 +15,23 @@ const Navbar = () => {
             href="https://flowbite.com/"
             class="flex items-center space-x-3 rtl:space-x-reverse"
           >
-            <img
+            <Image
               src="https://flowbite.com/docs/images/logo.svg"
               class="h-8"
               alt="Flowbite Logo"
+              width="40"
+              height="40"
             />
             <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
               Gitsplit
             </span>
           </a>
-          <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-            <button
-              type="button"
-              class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-            >
-              Login
-            </button>
-            <button
-              data-collapse-toggle="navbar-cta"
-              type="button"
-              class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-              aria-controls="navbar-cta"
-              aria-expanded="false"
-            >
-              <span class="sr-only">Open main menu</span>
-              <svg
-                class="w-5 h-5"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 17 14"
-              >
-                <path
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M1 1h15M1 7h15M1 13h15"
-                />
-              </svg>
-            </button>
-          </div>
           <div
             class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
             id="navbar-cta"
           >
             <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-              <li>
+              <li className="flex items-center">
                 <a
                   href="#"
                   class="block py-2 px-3 md:p-0 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:dark:text-blue-500"
@@ -65,7 +40,7 @@ const Navbar = () => {
                   Home
                 </a>
               </li>
-              <li>
+              <li className="flex items-center">
                 <a
                   href="#"
                   class="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
@@ -74,10 +49,31 @@ const Navbar = () => {
                 </a>
               </li>
               <li>
-                <div>
-                  <ConnectButton />
-                </div>{" "}
+                <ConnectButton />
               </li>
+              {!session ? (
+                <li>
+                  <Link href="/api/auth/signin/github">
+                    <button
+                      type="button"
+                      class="text-white h-full font-bold rounded-xl bg-blue-500 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-500 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                    >
+                      Login with github
+                    </button>
+                  </Link>
+                </li>
+              ) : (
+                <li>
+                  <Link href="/api/auth/signout/github">
+                    <button
+                      type="button"
+                      class="text-white h-full font-bold rounded-xl bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-500 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                    >
+                      Signout from github
+                    </button>
+                  </Link>
+                </li>
+              )}
             </ul>
           </div>
         </div>
