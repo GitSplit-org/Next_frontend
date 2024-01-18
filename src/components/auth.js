30a08442d8c16a408427d512b09b12001037d9c4
Punarv Pawade <punarvpawade17@gmail.com>
2024-01-18 23:44:44 +0530

    auth

---
+++ src/components/auth.js
@@ -0,0 +1,14 @@
+import { NextAuthOptions } from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+export const authOptions = {
+  // Secret for Next-auth, without this JWT encryption/decryption won't work
+  secret: process.env.NEXT_PUBLIC_NEXTAUTH_SECRET,
+
+  // Configure one or more authentication providers
+  providers: [
+    GithubProvider({
+      clientId: process.env.NEXT_PUBLIC_GITHUB_APP_CLIENT_ID,
+      clientSecret: process.env.NEXT_PUBLIC_GITHUB_APP_CLIENT_SECRET,
+    }),
+  ],
+};
